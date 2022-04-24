1b3cc55b212d877830138495f3c7fac3bd8d9d6a
vicnvx <victormlira@outlook.com>
2022-04-23 21:21:30 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-// search node_modules for 'express'
+// search node_modules and add them
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 
 const mockUserData = [
@@ -15,6 +18,35 @@ app.get('/users', function(req, res) {
         message: 'successfully got users. Nice!',
         users: mockUserData
     })
+})
+
+app.get('/users/:id', function(req, res) {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
+app.post('.login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsernname = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true, 
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
 })
 
 app.listen(8000, function(){
